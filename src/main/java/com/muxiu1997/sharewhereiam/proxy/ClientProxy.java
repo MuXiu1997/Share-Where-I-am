f35475ed99c75af463dace2cf97190380f5f0d7a
MuXiu1997 <muxiu1997@gmail.com>
2022-05-13 23:48:30 +0800

    Add CommandLoader

--- src/main/java/com/muxiu1997/sharewhereiam/proxy/ClientProxy.java
+++ src/main/java/com/muxiu1997/sharewhereiam/proxy/ClientProxy.java
@@ -1,20 +1,13 @@
 package com.muxiu1997.sharewhereiam.proxy;
 
-import com.muxiu1997.sharewhereiam.command.CommandWaypointSave;
-import com.muxiu1997.sharewhereiam.command.CommandWaypointShareLocation;
-import com.muxiu1997.sharewhereiam.command.CommandWaypointToggleTempBeacon;
-import com.muxiu1997.sharewhereiam.journeymap.WaypointManager;
+import com.muxiu1997.sharewhereiam.loader.CommandLoader;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
-import net.minecraftforge.client.ClientCommandHandler;
-import net.minecraftforge.common.MinecraftForge;
 
 @SuppressWarnings("unused")
 public class ClientProxy extends CommonProxy {
     @Override
     public void postInit(FMLPostInitializationEvent event) {
         super.postInit(event);
-        ClientCommandHandler.instance.registerCommand(CommandWaypointSave.INSTANCE);
-        ClientCommandHandler.instance.registerCommand(CommandWaypointShareLocation.INSTANCE);
-        ClientCommandHandler.instance.registerCommand(CommandWaypointToggleTempBeacon.INSTANCE);
+        CommandLoader.INSTANCE.clientLoad();
     }
 }
